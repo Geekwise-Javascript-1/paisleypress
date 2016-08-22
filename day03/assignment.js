825b1f92f28d4a245d34e5043ac7acc112cb8df6
paisleypress <paisleypress@yahoo.com>
2016-08-22 13:22:13 -0700

    changes made to clean up classwork functionality

--- day03/assignment.js
+++ day03/assignment.js
@@ -7,3 +7,12 @@ nameFirstLtr = nameFirstLtr.toUpperCase();
 lengthName = name.length;
 newName = nameFirstLtr + name.substring(1,lengthName);
 console.log(newName);
+
+
+name = capitalize(n)
+
+function capitalize(n){
+    return n.charAt(0).toUpperCase + n.substr(1, n.length);
+}
+
+console.log(name);
